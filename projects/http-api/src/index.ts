ec11960c6c7847bf3ed6dc60fd2b8378bd6ca28e
Jelani Jackson <jeljack86@gmail.com>
2020-09-22 21:49:12 -0500

    Implemented koa and serverless-http

--- projects/http-api/src/index.ts
+++ projects/http-api/src/index.ts
@@ -1,5 +1,24 @@
 import { Context } from "aws-lambda";
+import Koa from "koa";
+import Router from "@koa/router";
+import ServerlessHttp from "serverless-http";
+
+const app = new Koa();
+
+const router = new Router();
+
+router.get("/", async (ctx) => {
+  ctx.body = "This is a GET";
+});
+
+router.post("/", async (ctx) => {
+  ctx.body = "This is a POST";
+});
+
+app.use(router.routes());
+
+const handler = ServerlessHttp(app);
 
 exports.handler = async (event: any, context: Context) => {
-  return "Hello World!!!";
+  return await handler(event, context);
 };

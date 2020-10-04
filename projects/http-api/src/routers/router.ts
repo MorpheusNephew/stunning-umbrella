eeeeb213fb2e3dc28ba2c4426b1e22c024af1b42
Jelani Jackson <jeljack86@gmail.com>
2020-10-04 18:15:22 -0500

    Constructing router via the builder pattern

--- projects/http-api/src/routers/router.ts
+++ projects/http-api/src/routers/router.ts
@@ -3,8 +3,7 @@ import Router from "@koa/router";
 import { postsRouter } from "./posts-router";
 import { usersRouter } from "./users-router";
 
-export const router = new Router();
-
-router.prefix("/api/v1");
-router.use(postsRouter.routes());
-router.use(usersRouter.routes());
+export const router = new Router()
+  .prefix("/api/v1")
+  .use(postsRouter.routes())
+  .use(usersRouter.routes());

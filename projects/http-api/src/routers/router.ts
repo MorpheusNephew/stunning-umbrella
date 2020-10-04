c47200d8f483101280b3b83d570894168e1ad916
Jelani Jackson <jeljack86@gmail.com>
2020-10-04 17:37:55 -0500

    Setting a prefix for all routes

--- projects/http-api/src/routers/router.ts
+++ projects/http-api/src/routers/router.ts
@@ -5,5 +5,6 @@ import { usersRouter } from "./users-router";
 
 export const router = new Router();
 
+router.prefix("/api/v1");
 router.use(postsRouter.routes());
 router.use(usersRouter.routes());

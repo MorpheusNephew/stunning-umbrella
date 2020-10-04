0bf2c66803681aef83e92be0f6615e8711f5e5b3
Jelani Jackson <jeljack86@gmail.com>
2020-10-04 06:24:57 -0500

    Created policy for logging to cloudwatch for lambda

--- projects/http-api/main.tf
+++ projects/http-api/main.tf
@@ -38,6 +38,35 @@ resource "aws_iam_role" "iam_for_http_api_backend" {
 EOF
 }
 
+# Creating policy for lambda logging
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+# Attaching policy to role
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.iam_for_http_api_backend.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
 # Creating backend lambda function
 resource "aws_lambda_function" "backend" {
   function_name    = "snazzy-api"
